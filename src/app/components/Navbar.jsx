784b4ab8aacd6181c5248b5a0e57c9a8616cbd8e
rajalamdev <rajalamofficial@gmail.com>
2023-07-01 01:26:40 +0700

    beta

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -73,11 +73,11 @@ import { usePathname } from "next/navigation";
   
   return (
     <header>
-        <nav className="flex fixed transition-all w-full justify-between py-2 items-center px-6 md:px-12 z-40">
+        <nav className="flex fixed transition-all w-full justify-between py-1 items-center px-6 md:px-12 z-40">
           <motion.div variants={logo} transition={{
             duration: .2
           }} initial="hidden" animate="visible" className="flex gap-2 items-center scale-75 md:scale-100">
-            <Image src={"/logo.png"} alt="logo 21cleanshoes" width={60} height={60} className='scale-[.8]' />
+            <Image src={"/logo.png"} alt="logo 21cleanshoes" width={70} height={70} className='scale-[.8]' />
           </motion.div>
           <motion.div variants={nav} initial="closed" animate="open" className={`md:flex gap-12 text-md font-medium hidden`}>
             {navLinks.map(link => {
